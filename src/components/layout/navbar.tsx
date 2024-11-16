cde54be0c8fd305c388eda54116deba060b6a88f
muttyqt15 <hayaye69@gmail.com>
2024-11-17 02:37:48 +0700

    feat: navbar

---
+++ src/components/layout/navbar.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import {
+  ChevronDown,
+  Heart,
+  Search,
+  ShoppingBasket,
+  UserRound,
+} from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full h-fit font-poppins">
+      <div className="bg-[#001F54] w-full h-24 flex items-center justify-center">
+        <h5 className="text-white font-bold text-xs">
+          GET YOUR STYLE WITH JINISO! DISKON UP TO 70% + DISKON 20RB!
+        </h5>
+      </div>
+      <div className="p-8 xl:px-24 flex flex-col gap-8">
+        <div className="flex justify-between items-center">
+          <div className="bg-purple-900 rounded-full size-8" />
+          <div className="w-1/3 flex justify-end gap-[2em]">
+            <Heart className="hover:text-purple-900 hover:scale-[0.98] animate-ease cursor-pointer" />
+            <ShoppingBasket className="hover:text-purple-900 hover:scale-[0.98] animate-ease cursor-pointer" />
+            <UserRound className="hover:text-purple-900 hover:scale-[0.98] animate-ease cursor-pointer" />
+          </div>
+        </div>
+        <div className="flex justify-between gap-6">
+          <div className="flex gap-8 text-sm text-[#1E1E1E] font-medium">
+            <DropdownMenu>
+              <DropdownMenuTrigger className="relative flex gap-2 items-center min-w-fit">
+                <p>Shop By Categories</p>
+                <ChevronDown className="size-4" />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem>Billing</DropdownMenuItem>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+                <DropdownMenuItem>Subscription</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <button className="min-w-fit">Best Seller</button>
+            <button className="min-w-fit">Sale</button>
+            <button className="min-w-fit">Jiniso Joyful</button>
+          </div>
+          <div className="h-16 border max-w-[50%] w-full border-slate-400 bg-slate-300 pl-4 flex justify-between items-center p-2">
+            <input
+              type="text"
+              className="font-medium text-sm placeholder:text-slate-500 text-black w-full bg-transparent outline-none"
+              placeholder="Search..."
+            />
+            <Search className="size-8" />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
